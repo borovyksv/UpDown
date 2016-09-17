ae3fd78adab7eea4e9d02a268ac2129747929bfa
relzet <relzet69@gmail.com>
2016-09-18 00:47:35 +0300

    fixed problems with idea

--- src/main/java/com/relzet/service/UserDetailsServiceImpl.java
+++ src/main/java/com/relzet/service/UserDetailsServiceImpl.java
@@ -1,31 +1,31 @@
-//package com.relzet.service;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//@Service
-//public class UserDetailsServiceImpl implements UserDetailsService {
-//    @Autowired
-//    private UserService userService;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-//        com.relzet.model.User customUser = userService.findUserByLogin(login);
-//        if (customUser == null)
-//            throw new UsernameNotFoundException(login + " not found");
-//
-//        Set<GrantedAuthority> roles = new HashSet<>();
-//        roles.add(new SimpleGrantedAuthority("USER"));
-//
-//        return new User(customUser.getLogin(), customUser.getPassword(), roles);
-//    }
-//}
+package com.relzet.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        com.relzet.model.User customUser = userService.findUserByLogin(login);
+        if (customUser == null)
+            throw new UsernameNotFoundException(login + " not found");
+
+        Set<GrantedAuthority> roles = new HashSet<>();
+        roles.add(new SimpleGrantedAuthority("USER"));
+
+        return new User(customUser.getLogin(), customUser.getPassword(), roles);
+    }
+}
